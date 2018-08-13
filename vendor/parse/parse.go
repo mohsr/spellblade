69a7e27f3c9f2716a0c099f067e2d8e211fbe45f
mohsr <mkrizvi98@gmail.com>
2018-08-12 22:50:27 -0600

    added notes on format of a command

--- vendor/parse/parse.go
+++ vendor/parse/parse.go
@@ -1,5 +1,33 @@
 package parse
 
+import (
+    "strings"
+)
+
+/*
+ * NOTES:
+ * A text command is either a MENU ACTION or a GAME ACTION.
+ *
+ * A command is recognized as a MENU ACTION if the first word is a recognized 
+ * menu action. The format for a menu action is as follows:
+ *                   >>> [MENU_ACTION]
+ * An example menu action is as follows:
+ *                   >>> logout
+ * If the command is not a recognized menu action, it is treated as a
+ * game action. 
+ *
+ * A GAME ACTION is made up of three parts:
+ *                   >>> [VERB][FILLER][TARGET]
+ * The VERB is always necessary and will determine the action done.
+ * The FILLER is never necessary and consists of words like "the" and "a", and
+ * can be a variable amount of words.
+ * The TARGET is usually necessary and will determine the target of the verb.
+ * Example game actions are as follows:
+ *                   >>> attack the dragon
+ *                   >>> move north
+ *                   >>> search
+ */
+
 /*
  * Holds the response of parsing a text command.
  * Schema: {"Text":  The response text to send back.
@@ -21,6 +49,9 @@ type Response struct {
  *
  */
 func Parse(res *Response, txt string) {
+    /* Break up the words. */
+    words := strings.Fields(txt)
+
     res.Text = txt
     res.Color = "White"
 }
