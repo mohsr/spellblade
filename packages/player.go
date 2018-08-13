420c956fdc65a8cdc42579125ac38f55c3ccfeca
mohsr <mkrizvi98@gmail.com>
2018-08-12 21:55:11 -0600

    trying to figure out how local packages work on heroku

---
+++ packages/player.go
@@ -0,0 +1,30 @@
+package player
+
+/*
+ * Represents a player in the game.
+ * Schema: {"Name":  The name of the character.
+ *          "Class": The class of the character.
+ *          "Height": The height of the character.
+ *          "Hair": The hair color of the character.
+ *          "Level": The level of the character.}
+ */
+type Player struct {
+    Name   string
+    Class  string
+    Height string
+    Hair   string
+    Level  int
+}
+
+/*
+ * NewPlayer
+ * Purpose:    Generate a new player.
+ * Parameters: name - string - the name of the character.
+ *             class - string - the class of the character.
+ *             height - string - the height of the character.
+ *             hair - string - the hair color of the character.
+ */
+func NewPlayer(name string, class string, height string, hair string) Player {
+    p := Player{Name: name, Class: class, Height: height, Hair: hair, Level: 1}
+    return p
+}
